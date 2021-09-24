3e09d24d47034276330c798da59f501424b1c16d
ishwar <ishwardewangan6921@gmail.com>
2021-09-24 16:28:55 +0530

    add products page

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () => {
+    return (
+        <>
+            <h1>Cool product 01</h1>
+            <p>This is a very cool product</p>
+            <p>Rs. 2.50/-</p>
+            <hr />
+            <h1>Cool product 02</h1>
+            <p>This is a very cool product</p>
+            <p>Rs. 250.00</p>
+        </>
+    )
+}
+
+export default Products;
